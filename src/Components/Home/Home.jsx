f43a727438e182888c60eca619756867449d6474
trottafede <ftrotta18@gmail.com>
2022-02-19 02:49:05 -0300

    histories

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 
 import Feed from "../Feed/Feed";
+import Stories from "../Stories/Stories";
 
 function Home() {
   return (
@@ -12,6 +13,7 @@ function Home() {
         <div className="row">
           <div className="col-lg-4"></div>
           <div className="col-lg-4">
+            <Stories />
             <Feed />
           </div>
           <div className="col-lg-4">:V</div>
---
+++ src/Components/Stories/Stories.jsx
@@ -0,0 +1,65 @@
+import "./Stories.css";
+import React from "react";
+
+import trottafede from "../Feed/trottafede.jpg";
+function Stories() {
+  return (
+    <div id="Stories">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-2">
+            <div id="Story">
+              <div id="imgContainer">
+                <img className="userImg" src={trottafede} alt="user" />
+                <p>trottafede</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-2">
+            <div id="Story">
+              <div id="imgContainer">
+                <img className="userImg" src={trottafede} alt="user" />
+                <p>trottafede</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-2">
+            <div id="Story">
+              <div id="imgContainer">
+                <img className="userImg" src={trottafede} alt="user" />
+                <p>trottafede</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-2">
+            <div id="Story">
+              <div id="imgContainer">
+                <img className="userImg" src={trottafede} alt="user" />
+                <p>trottafede</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-2">
+            <div id="Story">
+              <div id="imgContainer">
+                <img className="userImg" src={trottafede} alt="user" />
+                <p>trottafede</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-2">
+            <div id="Story">
+              <div id="imgContainer">
+                <img className="userImg" src={trottafede} alt="user" />
+                <p>trottafede</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Stories;
